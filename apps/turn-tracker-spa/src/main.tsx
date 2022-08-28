ef21fe2c83b448a7f29a21eb53b075027591014f
Alec Bakholdin <alecbakholdin@gmail.com>
2022-08-28 13:52:21 -0400

    changed baseUrl in Router

--- apps/turn-tracker-spa/src/main.tsx
+++ apps/turn-tracker-spa/src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/app';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { CookiesProvider } from 'react-cookie';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'jotai';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {CookiesProvider} from 'react-cookie';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'jotai';
 import theme from './theme';
-import { SnackbarProvider } from 'notistack';
+import {SnackbarProvider} from 'notistack';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,11 +17,11 @@ root.render(
   <CookiesProvider>
     <ThemeProvider theme={theme}>
       <Provider>
-        <Router>
+        <Router basename={"/turn-tracker"}>
           <SnackbarProvider>
             <QueryClientProvider client={new QueryClient()}>
-              <CssBaseline />
-              <App />
+              <CssBaseline/>
+              <App/>
             </QueryClientProvider>
           </SnackbarProvider>
         </Router>
