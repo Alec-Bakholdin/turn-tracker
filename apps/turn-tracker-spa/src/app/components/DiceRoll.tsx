c02cacf7e111b4a0fad9db4acef6bc320ffedaf2
danieljliu <danieljliu3@gmail.com>
2022-09-20 23:28:26 -0400

    Resolves #1 Added DiceRoll component which rolls a D6 dice on click

--- apps/turn-tracker-spa/src/app/components/DiceRoll.tsx
+++ apps/turn-tracker-spa/src/app/components/DiceRoll.tsx
@@ -1,15 +1,17 @@
-import * as React from "react";
-import Dice from "react-dice-roll";
-import {Typography} from "@mui/material";
+import * as React from 'react';
+import Dice from 'react-dice-roll';
+import { Typography } from '@mui/material';
 
-//I have no idea what these commands do but Webstorm told me to put them in
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export default function DiceRoll({size,rollingTime,placement}) {
+export default function DiceRoll(props: {
+  size: number;
+  rollingTime: number;
+  placement: string;
+}) {
+  const { size, rollingTime, placement } = props;
   return (
     <div className="App">
-      <Typography className = "text-center">Roll D6</Typography>
-  <Dice size = {size} rollingTime = {rollingTime} placement = {placement}/>
-  </div>
-);
+      <Typography className="text-center">Roll D6</Typography>
+      <Dice size={size} rollingTime={rollingTime} placement={placement} />
+    </div>
+  );
 }
